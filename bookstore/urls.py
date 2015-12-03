946c6aaa27714786dad6882327987d95a1bc384b
Martin Duys <martinduys@gmail.com>
2015-12-03 11:22:12 +0200

    Add store

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
