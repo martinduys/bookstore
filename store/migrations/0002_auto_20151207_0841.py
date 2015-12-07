6795bd610d8b551bc857a159addd798ac84c6161
Martin Duys <martinduys@gmail.com>
2015-12-07 09:07:49 +0200

    added models

---
+++ store/migrations/0002_auto_20151207_0841.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('store', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='book',
+            name='price',
+            field=models.DecimalField(max_digits=8, default=0, decimal_places=2),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='stock',
+            field=models.IntegerField(default=0),
+        ),
+    ]
