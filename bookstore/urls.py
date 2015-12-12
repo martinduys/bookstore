2f2448133a24be4e93fd5a8354bffa48a3dc2ead
Martin Duys <martinduys@gmail.com>
2015-12-13 01:06:40 +0200

    Add social auth

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     url(r'^$', include('store.urls'), name='store'),
     url(r'^store/', include('store.urls'), name='store'),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url('', include('social.apps.django_app.urls', namespace = 'social')),
     url(r'^admin/', include(admin.site.urls)),
 ]
