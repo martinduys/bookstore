ea955dcc970d59b0f6d6c130f357838161ea2366
Martin Duys <martinduys@gmail.com>
2015-12-12 18:24:58 +0200

    registration templates

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Registration
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('store.urls'), name='store'),
     url(r'^store/', include('store.urls'), name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')), 
     url(r'^admin/', include(admin.site.urls)),
 ]
